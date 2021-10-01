5b51b3fa1743de30b56d783267e975ea68fbd0e8
ashkan20171 <ashkan.mataee@yahoo.com>
2021-10-01 18:23:05 +0330

    first commit

---
+++ tblLogin.sql
@@ -0,0 +1,23 @@
+USE [Accounting_DB]
+GO
+
+/****** Object:  Table [dbo].[Login]    Script Date: 07/02/2018 10:15:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Login](
+	[LoginID] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [nvarchar](150) NOT NULL,
+	[Password] [nvarchar](150) NOT NULL,
+ CONSTRAINT [PK_Login] PRIMARY KEY CLUSTERED 
+(
+	[LoginID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
